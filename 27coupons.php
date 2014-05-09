ff278f90cf953c7e91ffda50c9acc5bd006dfd56
diffion <diffion@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-09 09:05:10 +0000

    updated to 3.1
    git-svn-id: https://plugins.svn.wordpress.org/27coupons/trunk@910960 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 27coupons.php
+++ 27coupons.php
@@ -3,7 +3,7 @@
 Plugin Name: 27coupons
 Plugin URI: http://www.27coupons.com/wp-widget/
 Description: You can create a widgets which will display latest discount coupons and deals of Indian e-commerce websites from 27coupons.com.
-Version: 2.1
+Version: 3.0
 Author: diffion
 Author URI: http://www.diffion.com
 License: GPL2
@@ -25,21 +25,50 @@ License: GPL2
 */
 class CouponsWidget extends WP_Widget
 {
+  var $default_css='.CouponsWidget{	
+width:100%; 
+line-height:18px; 
+font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+font-size:14px;
+vertical-align:middle;
+min-height: 20px;
+margin-top:0px;
+margin-bottom: 10px;
+border-radius: 4px;
+-webkit-border-radius: 4px;
+-moz-border-radius: 4px;
+}
+.CouponsWidget h3{font-size:100%; margin-bottom:5px !important; font-weight:bold; line-height:24px;}
+.CouponsWidget h3{font-size:100%; margin-bottom:5px !important; font-weight:bold; line-height:24px;}
+.widget_27box{ }
+.small_27{font-size:10px; display:block;  text-align:center; font-weight:bold; padding-top:10px;	}
+.small_27 img{ vertical-align:middle; }
+.c-deals{padding-top:5px;padding-bottom:5px; display:block; font-weight:normal;}
+.c-store, .c-store a:link, .c-store a:visited {text-decoration:none; font-weight:normal;}
+.c-store a:hover{ text-decoration:none; font-weight:bold;}
+.c-coupon, .c-coupon a:link, .c-coupon a:visited {text-decoration:none; font-weight:normal; }
+.c-coupon a:hover{ text-decoration:none; font-weight:bold;}';
+    
   function CouponsWidget()
   {
-    $widget_ops = array('classname' => 'CouponsWidget', 'description' => 'Displays latest discount coupons and deals of Indian stores from 27coupons.com.' );
-    $this->WP_Widget('CouponsWidget', 'Latest Discount Coupons & Deals', $widget_ops);
+    $widget_ops = array('classname' => 'CouponsWidget', 'description' => 'Displays latest discount coupons of Indian shopping websites from 27coupons.com.' );
+    $this->WP_Widget('CouponsWidget', 'Latest Coupons', $widget_ops);
   }
  
   function form($instance)
   {
     $instance = wp_parse_args( (array) $instance, array( 'title' => '' ), array( 'api_key' => '' ) );
-    $title = $instance['title'];
+    //$title = $instance['title'];
 	$api_key = $instance['api_key'];
+    
+    $title = empty($instance['title']) ? 'Latest Coupons' : $instance['title'];
+    
+    $css_style = empty($instance['coupons27box_css']) ? $this->default_css : $instance['coupons27box_css'];
+    
 ?>
-  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: </label><input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></p>
   
-  <p><label for="<?php echo $this->get_field_id( 'num_coupons' ); ?>"><?php _e('Number of Deals:', 'widgetcoupons'); ?></label> 
+  <p><label for="<?php echo $this->get_field_id( 'num_coupons' ); ?>"><?php _e('Number of Coupons:', 'widgetcoupons'); ?></label> 
 			<select id="<?php echo $this->get_field_id( 'num_coupons' ); ?>" name="<?php echo $this->get_field_name( 'num_coupons' ); ?>" class="widefat" style="width:100%;">
 				<option <?php if ( '4' == $instance['num_coupons'] ) echo 'selected="selected"'; ?>>5</option>
 				<option <?php if ( '7' == $instance['num_coupons'] ) echo 'selected="selected"'; ?>>7</option>
@@ -48,7 +77,9 @@ class CouponsWidget extends WP_Widget
 	</select>
 	</p>
 	
-	<p><label for="<?php echo $this->get_field_id('api_key'); ?>">API Key: <input class="widefat" id="<?php echo $this->get_field_id('api_key'); ?>" name="<?php echo $this->get_field_name('api_key'); ?>" type="text" value="<?php echo attribute_escape($api_key); ?>" /></label></p>
+	<p><label for="<?php echo $this->get_field_id('api_key'); ?>">API Key: </label><input class="widefat" id="<?php echo $this->get_field_id('api_key'); ?>" name="<?php echo $this->get_field_name('api_key'); ?>" type="text" value="<?php echo attribute_escape($api_key); ?>" /></p>
+    
+    <p><label for="<?php echo $this->get_field_id('coupons27box_css'); ?>">CSS: </label><textarea class="widefat" id="<?php echo $this->get_field_id('coupons27box_css'); ?>" name="<?php echo $this->get_field_name('coupons27box_css'); ?>" rows="10"><?php echo $css_style; ?></textarea></p>
 	
 	<a style="text-decoration:none;" href="http://www.27coupons.com/wp-widget/" target="_blank">Click here to monetize this widget.</a>
 	
@@ -61,6 +92,7 @@ class CouponsWidget extends WP_Widget
     $instance['title'] = $new_instance['title'];
 	$instance['api_key'] = $new_instance['api_key'];
 	$instance['num_coupons'] = $new_instance['num_coupons'];
+    $instance['coupons27box_css'] = $new_instance['coupons27box_css'];
 	
     return $instance;
   }
@@ -69,23 +101,27 @@ class CouponsWidget extends WP_Widget
   {
     extract($args, EXTR_SKIP);
 	
-	echo $before_widget;
-    $title = empty($instance['title']) ? ' Latest Coupons & Deals ' : apply_filters('widget_title', $instance['title']);
+    $title = empty($instance['title']) ? 'Latest Coupons' : apply_filters('widget_title', $instance['title']);
 	$num_coupons = empty($instance['num_coupons']) ? '7' : $instance['num_coupons'];
-	$api_key = empty($instance['api_key']) ? '615e26b167eba5788883d18e8dfef0329013daa3d7f9619e92b6da2c1a3a32a8b5106bb8e792d78aa2e6cfb0069e1147a0ba718b134ff2817bd41235061f6bcf' : $instance['api_key'];
-
+	$api_key = empty($instance['api_key']) ? 'ba718b134ff2817bd41235061f6bcfcc73fa' : $instance['api_key'];
+    $css_style = empty($instance['coupons27box_css']) ? $this->default_css : $instance['coupons27box_css'];
+    
+    echo '<style type="text/css">'.$css_style.'</style>';	
+    
+    echo $before_widget;
+    
     if (!empty($title))
       echo $before_title . $title . $after_title;
 
     // WIDGET CODE GOES HERE
-	$api_url='http://api.27coupons.com/v1.0/wp/get-coupons/?key='.$api_key.'&limit='.$num_coupons;
+    $api_url='http://apis.27coupons.com/v1.0/wp/get-coupons/?key='.$api_key.'&limit='.$num_coupons;
 	$ch = curl_init($api_url);
 	curl_setopt($ch,  CURLOPT_HTTPGET, 1);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$coupons_html_code = curl_exec($ch);
-
+    
 	echo  $coupons_html_code;
  
     echo $after_widget;
--- readme.txt
+++ readme.txt
@@ -1,25 +1,26 @@
 === 27coupons ===
 Contributors: diffion
-Tags: 27coupons, discount coupons, shopping deals, india shopping, india deals, coupons, coupon code
+Tags: 27coupons, discount coupons, shopping deals, india shopping, shopping deals, coupons, coupon code
 Requires at least: 2.8.2
-Tested up to: 3.5.2
+Tested up to: 3.9.1
 
-This plugin will create a widget which will display latest discount coupons and deals of Indian e-commerce websites from 27coupons.com.
+This plugin will create a widget which will display latest discount coupons of Indian shopping websites from 27coupons.com.
 
 == Description ==
 
-This plugin will create a widget which will display latest discount coupons and deals of Indian e-commerce websites from 27coupons.com. 
+This plugin will create a widget which will display latest discount coupons of Indian shopping websites from 27coupons.com. 
 You can customize the plugin to change the title and number of deals to be displayed. 
+You can modify the css style to customize the look and feel to match yout template.
 You can also monetize this widget. Please visit our webiste to know more.
 For any customizations of this plugin, please contact us at http://www.27coupons.com/wp-widget/
-If you are interested in creating your own plugin using our coupons data check http://api.27coupons.com/.
+If you are interested in creating your own plugin using our coupons data check http://www.27coupons.com/api/.
 
 == Installation ==
 
 1. Download the plugin zip file (27coupons.zip) from http://cdn.27coupons.com/widgets/27coupons.zip
 2. Unzip and Upload contents of folder '27coupons' to the '/wp-content/plugins/' directory
 3. Activate the plugin through the 'Plugins' menu in WordPress
-4. Go to 'Appearance-->Widgets' and add the widget 'Latest Discount Coupons and Deals' to the sidebar.
+4. Go to 'Appearance-->Widgets' and add the widget 'Latest Coupons' to the sidebar.
 5. You can change the title and number of deals to be displayed.
 6. Enter your API Key to monetize this widget. Please visit our website to get your API key.
 
@@ -31,4 +32,5 @@ Please contact us at http://www.27coupons.com/wp-widget/ and we will help you in
 
 == Screenshots ==
 
-1. screenshot-1.png.
+1. widget_sample.png.
+2. widget_settings.png.
